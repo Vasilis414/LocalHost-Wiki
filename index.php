3fc6f9455c978510ea05b65c4e3037c447e81b21
Vasilis414 <83064774+Vasilis414@users.noreply.github.com>
2023-06-12 02:42:47 -0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+    
+    $_PASSWORD = "Your Password"
+
+
+
+
+
+    if ((string) $_POST[$_PASSWORD] == "193755") {
+        $_SESSION['login'] = true;
+    }
+    
+    if (isset($_SESSION['login'])) {
+        echo "success";
+        header("Location: ./contents/table.php");
+    }
+?>
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./style.css"> <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu"> </head>
+    <style> 
+    body {
+        font-family: "Ubuntu", sans-serif;
+    }
+    </style>
+<body> <center> 
+    <img src="./img/lock.png" class = "lock">
+    <br>
+    <strong><div class = "text"> LOCKED </div></strong>
+    <br>
+    <form action="./index.php" method="post">
+        <input class = "passfield" type="password" name="password" placeholder="" required="true">
+        <button class = "btnfield">ㅤ</button>
+    </form>
+</center></body>
